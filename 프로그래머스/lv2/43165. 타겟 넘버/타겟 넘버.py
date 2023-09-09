9400499444dc33c035b8762a7c9a8459e183d9b8
UnMun Lee <80229922+ttoro-lee@users.noreply.github.com>
2023-09-09 10:28:03 +0900

    Update 타겟 넘버.py
    주석 추가

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,7 +1,7 @@
 def solution(numbers, target):
     
     def dfs(idx, n):
-        nonlocal answer
+        nonlocal answer # local의 변수가 아님을 선언(global과 다름)
         
         if idx == len(numbers):
             if target == n:
